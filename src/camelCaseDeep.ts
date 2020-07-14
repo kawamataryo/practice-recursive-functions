c6f32b43cb606182774f24c5147fcc7ace474220
kawamataryou <ba068082@gmail.com>
2020-07-15 07:34:32 +0900

    add camelCaseDeep

--- src/camelCaseDeep.ts
+++ src/camelCaseDeep.ts
@@ -1,3 +1,19 @@
-export const camelCaseDeep = (object: any): any => {
-  return "";
+export const camelize = (str: string) => {
+  return str.split("_").reduce<string>((acc, cur, i): string => {
+    if (i === 0) {
+      return cur.toLowerCase();
+    }
+    return acc + cur.charAt(0).toUpperCase() + cur.slice(1).toLowerCase();
+  }, "");
+};
+
+export const camelCaseDeep = (obj: Record<string, any>) => {
+  const result = {} as Record<string, any>;
+  Object.keys(obj).forEach((key) => {
+    if (Object.prototype.toString.call(obj[key]) === "[object Object]") {
+      obj[key] = camelCaseDeep(obj[key]);
+    }
+    result[camelize(key)] = obj[key];
+  });
+  return result;
 };
--- tests/camelCaseDeep.test.ts
+++ tests/camelCaseDeep.test.ts
@@ -1,15 +1,54 @@
-import { camelCaseDeep } from "../src/camelCaseDeep";
+import { camelCaseDeep, camelize } from "../src/camelCaseDeep";
 
 describe("camelCaseDeep", () => {
-  xtest("キャメルケースに変換できる", () => {
-    const object = {
-      first_name: "taro",
-      last_name: "yamada",
-    };
+  describe("camelCaseDeep", () => {
+    test("キャメルケースに変換できる（1階層）", () => {
+      const object = {
+        first_name: "taro",
+        last_name: "yamada",
+      };
+      expect(camelCaseDeep(object)).toEqual({
+        firstName: "taro",
+        lastName: "yamada",
+      });
+    });
+    test("キャメルケースに変換できる（2階層）", () => {
+      const object = {
+        first_name: "taro",
+        last_name: "yamada",
+        has_profile: {
+          tel_number: "0805",
+          address: {
+            first_city: "mito",
+            second_city: "hitachinaka",
+            array: [2, 3, 4],
+          },
+        },
+      };
+      expect(camelCaseDeep(object)).toEqual({
+        firstName: "taro",
+        lastName: "yamada",
+        hasProfile: {
+          telNumber: "0805",
+          address: {
+            firstCity: "mito",
+            secondCity: "hitachinaka",
+            array: [2, 3, 4],
+          },
+        },
+      });
+    });
+  });
 
-    expect(camelCaseDeep(object)).toEqual({
-      firstName: "taro",
-      lastName: "jiro",
+  describe("camelize", () => {
+    test.each([
+      ["snake_case", "snakeCase"],
+      ["snake_case_case", "snakeCaseCase"],
+      ["snake_case_case_case", "snakeCaseCaseCase"],
+      ["SNAKE_CASE_CASE_CASE", "snakeCaseCaseCase"],
+      ["snake", "snake"],
+    ])("camelize %s", (str, expected) => {
+      expect(camelize(str)).toBe(expected);
     });
   });
 });
